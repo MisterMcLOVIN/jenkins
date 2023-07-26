351e2ea33dc78f3224aef43b3ceb2f88589802e4
Luka Jashi <lukajashi13@gmail.com>
2023-07-27 00:18:11 +0400

    jenkins homework files

---
+++ src/test/java/SwoopTest.java
@@ -0,0 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SwoopTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void logInTest() {
+        driver.get("https://www.swoop.ge/");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement logInButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='HeaderTools swoop-login']/p")));
+        logInButton.click();
+
+        WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='close-button']/a/img")));
+        closeButton.click();
+    }
+
+}
